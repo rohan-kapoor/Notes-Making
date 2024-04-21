f4dcfa7e1e70808bebacc2888a1fa5f742bfbd02
rohan-kapoor <kapoorrohan2004@gmail.com>
2024-04-21 22:17:33 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NotesList from './components/NotesList'
+import {useState} from 'react'
+import {nanoid} from'nanoid'
+import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [notes, setNotes] = useState([
+  {
+    id: nanoid(),
+    text: 'This is my first note!',
+    date: '15/04/2024',
+  },
+  {
+    id: nanoid(),
+    text: 'This is my second note!',
+    date: '15/04/2024',
+  },
+  {
+    id: nanoid(),
+    text: 'This is my third note!',
+    date: '15/04/2024',
+  },
+]);
+
+const [searchText, setSearchText] = useState('');
+
+const addNote = (text) => {
+  const date = new Date();
+  const newNote = {
+    id: nanoid(),
+    text: text,
+    date: date.toLocaleDateString()
+  }
+  const newNotes = [...notes, newNote];
+  setNotes(newNotes);
 }
 
-export default App;
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note)=> note.id !== id);
+  setNotes(newNotes);
+}
+  return (
+    <div className='container'>
+      <Search handleSearchNote={setSearchText}/>
+      <NotesList
+      notes = {notes.filter((note)=>
+        note.text.toLowerCase().includes(searchText)
+      )}
+      handleAddNote={addNote}
+      handleDeleteNote={deleteNote}/>
+    </div>
+  )
+}
+
+export default App
